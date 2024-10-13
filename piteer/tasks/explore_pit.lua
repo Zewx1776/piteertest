88b0ce849f6b53ee0c51d557b0ff61c4793f4dcb
Zewx <83424771+Zewx1776@users.noreply.github.com>
2024-10-12 20:18:18 -0700

    Add files via upload

--- piteer/tasks/explore_pit.lua
+++ piteer/tasks/explore_pit.lua
@@ -5,7 +5,7 @@ local explorer = require "core.explorer"
 local task  = {
     name = "Explore Pit",
     shouldExecute = function()
-        return utils.player_on_quest(enums.quests.pit_ongoing) and 
+        return -- utils.player_on_quest(enums.quests.pit_ongoing) and 
                not utils.get_closest_enemy() and
                not explorer.is_task_running
     end,
