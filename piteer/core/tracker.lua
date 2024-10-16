24ce368bc3f0ef1d30efb5df065b0a1f1df23584
Zewx <83424771+Zewx1776@users.noreply.github.com>
2024-10-15 20:14:18 -0700

    SALVAGE WIP

--- piteer/core/tracker.lua
+++ piteer/core/tracker.lua
@@ -1,7 +1,29 @@
 local tracker = {
     finished_time = 0,
     pit_start_time = 0,
-    boss_task_running = false,  -- Renamed field
+    boss_task_running = false,  
+    finished_time = 0,
+    pit_start_time = 0,
+    ga_chest_opened = false,
+    selected_chest_opened = false,
+    gold_chest_opened = false,
+    finished_chest_looting = false,
+    has_salvaged = false,
+    exit_horde_start_time = 0,
+    has_entered = false,
+    start_dungeon_time = nil,
+    horde_opened = false,
+    first_run = false,
+    exit_horde_completion_time = 0,
+    exit_horde_completed = true,
+    wave_start_time = 0,
+    needs_salvage = false,
+    victory_lap = false,
+    victory_positions = nil,
+    locked_door_found = false,
+    boss_killed = false,
+    teleported_from_town = false,
+    start_time = 0
 }
 
 -- Add these functions to manage the flag
@@ -11,6 +33,10 @@ end
 
 function tracker:is_boss_task_running()
     return self.boss_task_running
+end
+
+function tracker.set_teleported_from_town(value)
+    tracker.teleported_from_town = value
 end
 
 return tracker
