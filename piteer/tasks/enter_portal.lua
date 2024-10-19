ffca8d0c22f5ea56713430117a6c15c8cade4f57
Zewx <83424771+Zewx1776@users.noreply.github.com>
2024-10-19 12:33:39 -0700

    Add files via upload

--- piteer/tasks/enter_portal.lua
+++ piteer/tasks/enter_portal.lua
@@ -2,19 +2,20 @@ local utils = require "core.utils"
 local enums = require "data.enums"
 local explorer = require "core.explorer"
 local tracker = require "core.tracker"
+local settings = require "core.settings"  -- Make sure to include settings
 
 local task  = {
     name = "Enter Portal",
     shouldExecute = function()
-        --console.print("Checking if the task should be executed.")
         return utils.get_pit_portal()
     end,
     Execute = function()
         console.print("Executing the task: Enter Portal.")
         local portal = utils.get_pit_portal()
         if portal then
-            if utils.player_on_quest(enums.quests.pit_ongoing) then
-                console.print("Player is on the quest 'pit_ongoing'. Clearing path and setting target to portal.")
+            local is_player_in_pit = (utils.player_in_zone("EGD_MSWK_World_02") or utils.player_in_zone("EGD_MSWK_World_01")) and settings.enabled
+            if is_player_in_pit then
+                console.print("Player is in the pit zone. Clearing path and setting target to portal.")
                 explorer:clear_path_and_target()
                 explorer:set_custom_target(portal:get_position())
                 explorer:move_to_target()
@@ -26,7 +27,7 @@ local task  = {
                     tracker.start_location_reached = false
                 end
             else
-                console.print("Player is not on the quest 'pit_ongoing'. Interacting with the portal using loot manager.")
+                console.print("Player is not in the pit zone. Interacting with the portal using loot manager.")
                 loot_manager.interact_with_object(portal)
                 tracker.start_location_reached = false
             end
