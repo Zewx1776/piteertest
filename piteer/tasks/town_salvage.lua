bd45c224c41160a07a98ccba18864f5d5b6402fd
Zewx <83424771+Zewx1776@users.noreply.github.com>
2024-10-23 12:32:24 -0600

    Update town_salvage.lua

--- piteer/tasks/town_salvage.lua
+++ piteer/tasks/town_salvage.lua
@@ -24,7 +24,11 @@ local uber_table = {
     { name = "Doombringer", sno = 221017 },
     { name = "Harlequin Crest", sno = 609820 },
     { name = "Melted Heart of Selig", sno = 1275935 },
-    { name = "‍Ring of Starless Skies", sno = 1306338 }
+    { name = "‍Ring of Starless Skies", sno = 1306338 },
+    { name = "Shroud of False Death", sno = 2059803 },
+    { name = "Nesekem, the Herald", sno = 1982241 },
+    { name = "Heir of Perdition", sno = 2059799 },
+    { name = "Shattered Vow", sno = 2059813 }
 }
 
 local function is_uber_item(sno_to_check)
